90d87b9c7bc9eba311193df40398e35032561b90
oyeniyi Ridwan Adebara <oyeniyiridwan5@gmail.com>
2022-11-10 12:42:37 +0100

    Detail page testing

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,15 +1,164 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
+  static const routeHomePage = "/";
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<String> pics = [
+    "images/goat.jpg",
+    "images/delete.jfif",
+    "images/bike7.png"
+  ];
+  int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: SingleChildScrollView(
+        physics: ScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+        dragStartBehavior: DragStartBehavior.start,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: SizedBox(
+                height: 80,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      icon: Icon(
+                        Icons.arrow_back,
+                        color: Theme.of(context).iconTheme.color,
+                      ),
+                      onPressed: () {},
+                    ),
+                    Text("Title",
+                        style: Theme.of(context).textTheme.titleMedium),
+                    SizedBox(width: 50),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+              child: SizedBox(
+                  height: 200,
+                  width: width,
+                  child: Stack(
+                    // fit: StackFit.expand,
+                    children: [
+                      SizedBox(
+                        height: 200,
+                        width: width,
+                        child: Image(
+                          image: AssetImage(pics[_currentIndex]),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Positioned(
+                        left: 10,
+                        bottom: 75,
+                        child: CircleAvatar(
+                          backgroundColor: Colors.grey,
+                          radius: 25,
+                          child: Center(
+                            child: IconButton(
+                              icon: Icon(
+                                Icons.arrow_back_ios,
+                                color: Theme.of(context).iconTheme.color,
+                              ),
+                              onPressed: () {
+                                if (_currentIndex > 0) {
+                                  setState(() {
+                                    _currentIndex--;
+                                  });
+                                }
+                              },
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        right: 10,
+                        bottom: 75,
+                        child: CircleAvatar(
+                          backgroundColor: Colors.grey,
+                          radius: 25,
+                          child: Center(
+                            child: IconButton(
+                              icon: Icon(
+                                Icons.arrow_forward_ios,
+                                color: Theme.of(context).iconTheme.color,
+                              ),
+                              onPressed: () {
+                                if (_currentIndex < (pics.length - 1)) {
+                                  setState(() {
+                                    _currentIndex++;
+                                  });
+                                }
+                              },
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  )),
+            ),
+            details(context, "Population", "ade"),
+            details(context, "Region", "ade"),
+            details(context, "Capital", "ade"),
+            details(context, "Motto", "ade"),
+            const SizedBox(
+              height: 25,
+            ),
+            details(context, "Official language", "ade"),
+            details(context, "Ethic group", "ade"),
+            details(context, "Religion", "ade"),
+            details(context, "Government", "ade"),
+            const SizedBox(
+              height: 20,
+            ),
+            details(context, "Independence", "ade"),
+            details(context, "Area", "ade"),
+            details(context, "Currency", "ade"),
+            details(context, "GDP", "ade"),
+            const SizedBox(
+              height: 20,
+            ),
+            details(context, "Time zone", "ade"),
+            details(context, "Date format", "ade"),
+            details(context, "Dialing code", "ade"),
+            details(context, "Driving side", "ade"),
+            const SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
+      ),
+    );
   }
+}
+
+Widget details(BuildContext context, String title, String details) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 20),
+    child: Row(
+      children: [
+        Text(
+          "$title:  ",
+          style: Theme.of(context).textTheme.bodyMedium,
+        ),
+        Text(details, style: Theme.of(context).textTheme.bodySmall),
+      ],
+    ),
+  );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:countries_app/Details.dart';
 import 'package:countries_app/homePage.dart';
 import 'package:flutter/material.dart';
 
@@ -13,19 +14,31 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted
+          iconTheme: const IconThemeData(color: Colors.white),
+          textTheme: const TextTheme(
+            titleMedium: TextStyle(color: Colors.white, fontSize: 25),
+            bodySmall: TextStyle(
+                color: Colors.white, fontSize: 17, fontWeight: FontWeight.w300),
+            bodyMedium: TextStyle(
+                color: Colors.white, fontSize: 17, fontWeight: FontWeight.bold),
+          ),
+          scaffoldBackgroundColor: const Color(0xff000f24),
+          shadowColor: Colors.transparent),
+      routes: {
+        MyHomePage.routeHomePage: (context) => MyHomePage(),
+        DetailPage.routeDetailPage: (context) => DetailPage()
+      },
     );
   }
 }
